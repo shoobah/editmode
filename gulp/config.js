2ad8d015ae9f81fd1f348c531ad89745b0de3d5d
shoobah <shoobah@gmail.com>
2015-02-06 11:01:45 +0100

    Ritar lite bars

--- gulp/config.js
+++ gulp/config.js
@@ -12,7 +12,7 @@ module.exports = {
     src: src + "/sass/*.{sass,scss}",
     dest: dest,
     settings: {
-      indentedSyntax: true, // Enable .sass syntax!
+      indentedSyntax: false, // Disable .sass syntax!
       imagePath: 'images' // Used by the image-url helper
     }
   },
@@ -43,21 +43,11 @@ module.exports = {
     // A separate bundle will be generated for each
     // bundle config in the list below
     bundleConfigs: [{
-      entries: src + '/javascript/global.coffee',
-      dest: dest,
-      outputName: 'global.js',
-      // Additional file extentions to make optional
-      extensions: ['.coffee', '.hbs'],
-      // list of modules to make require-able externally
-      require: ['jquery', 'backbone/node_modules/underscore']
-      // See https://github.com/greypants/gulp-starter/issues/87 for note about
-      // why this is 'backbone/node_modules/underscore' and not 'underscore'
-    }, {
       entries: src + '/javascript/page.js',
       dest: dest,
       outputName: 'page.js',
       // list of externally available modules to exclude from the bundle
-      external: ['jquery', 'underscore']
+      external: ['jquery']
     }]
   },
   production: {
--- src/javascript/page.js
+++ src/javascript/page.js
@@ -1,21 +1,54 @@
 // Browserify entry point for the page.js bundle (yay JavaScript!)
-
-var $ = require('jquery');
-var _ = require('underscore');
+var d3 = require('d3');
 // global.js already contains jQuery, so in our config.js file, we
 // are exposing it to other files like this one in the `require` array.
 // Also in config.js, jquery is listed in `external` array for this bundle.
 // This combination lets this file use the jquery module bundled with
 // global.js, instead including it twice!
 
-var messageTemplate = _.template("<p>Made with <%= feels %> at <a href='<%= url %>'><%= bestCompanyEvar %>!</a></p>");
+var barData = (function() {
+  var randomValues = [];
+  for (var i = 0; i < 100; i++) {
+    var max = 100,
+      min = 5;
+    randomValues.push(Math.floor(Math.random() * (max - min)) + min);
+  }
+  return randomValues;
+}());
 
-var message = messageTemplate({
-  bestCompanyEvar: 'Viget',
-  feels: '♥',
-  url: 'http://viget.com'
-});
+var width = 900,
+  height = 500;
 
-$('body').append(message);
+var yScale = d3.scale.linear()
+  .domain([0, d3.max(barData)])
+  .range([0, height]);
 
-console.log('page.js loaded!');
+var xScale = d3.scale.ordinal()
+  .domain(d3.range(0, barData.length))
+  .rangeBands([0, width], 0.2);
+
+var colors = d3.scale.linear().domain([0, barData.lenth]).range(['#0000ff', '#ff0000']);
+
+console.log('barData', barData);
+d3.select('#content')
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height)
+  .style('background', '#eeeeee')
+  .selectAll('rect').data(barData)
+  .enter().append('rect')
+  .attr('x', function(d, i) {
+    return xScale(i);
+  })
+  .attr('y', function(d) {
+    return height - yScale(d);
+  })
+  .attr('width', xScale.rangeBand())
+  .attr('height', function(d) {
+    return yScale(d);
+  })
+  .attr('rx', 0)
+  .attr('ry', 0)
+  .style('fill', function(d, i) {
+    return colors(i);
+  });
